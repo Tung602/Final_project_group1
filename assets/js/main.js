c334d64f9f29d60dc6bd330e4942cf89b6c86d14
Tung <dothanhtungnb602@gmail.com>
2022-03-27 22:26:44 +0700

    add header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
-var navbarBtn = document.getElementById("navbar-toggler");
-var navbar = document.getElementById("navbar");
-var modal = document.getElementById("modal");
-navbarBtn.onclick = () => {
-    navbar.classList.add("show-navbar");
+// ============ Header ================
+
+// Menu aside
+let navbarToggler = document.querySelector(".navbar-toggler");
+let menuAside = document.querySelector(".menu-wrapper");
+let modal = document.querySelector(".modal");
+navbarToggler.onclick = () => {
+    menuAside.classList.add("show-menu-aside");
     modal.classList.add("show-modal");
-};
+}
 modal.onclick = () => {
-    navbar.classList.remove("show-navbar");
+    menuAside.classList.remove("show-menu-aside");
     modal.classList.remove("show-modal");
 }
