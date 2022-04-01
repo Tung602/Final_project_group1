58b10816aa0b0217e7c1d01bc327aa7c5f777dec
Tung <dothanhtungnb602@gmail.com>
2022-04-02 01:59:06 +0700

    fix something

---
+++ assets/js/admin.js
@@ -0,0 +1,17 @@
+// =============== Admin ===============
+
+let tabLinks = document.getElementsByClassName("admin-nav-btn");
+let tabContent = document.getElementsByClassName("nothing");
+for(let i=0;i<tabLinks.length;i++){
+  tabLinks[i].onclick = () =>{
+    for(let j=0;j<tabContent.length;j++){
+      tabContent[j].style.display = "none";
+    }
+    for(let j=0;j<tabLinks.length;j++){
+      tabLinks[j].classList.remove("active");
+    }
+    tabLinks[i].classList.add("active");
+    tabContent[i].style.display = "block";
+  }
+}
+tabLinks[0].click();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,123 +1,105 @@
-// // ============ Header ================
+// ============ Header ================
 
-// // Menu aside
-// let navbarToggler = document.querySelector(".main-navbar-toggler");
-// let menuAside = document.querySelector(".menu-wrapper");
-// let modal = document.querySelector(".main-modal");
-// navbarToggler.onclick = () => {
-//   menuAside.classList.add("show-menu-aside");
-//   modal.classList.add("show-modal");
-// };
-// modal.onclick = () => {
-//   menuAside.classList.remove("show-menu-aside");
-//   modal.classList.remove("show-modal");
-// };
+// Menu aside
+let navbarToggler = document.querySelector(".main-navbar-toggler");
+let menuAside = document.querySelector(".menu-wrapper");
+let modal = document.querySelector(".main-modal");
+navbarToggler.onclick = () => {
+  menuAside.classList.add("show-menu-aside");
+  modal.classList.add("show-modal");
+};
+modal.onclick = () => {
+  menuAside.classList.remove("show-menu-aside");
+  modal.classList.remove("show-modal");
+};
 
-// // =============== Main ==================
+// =============== Main ==================
 
-// // Slider
-// let heroBg = document.querySelector(".hero-bg");
-// let sliderBtnNext = document.querySelector(".hero-next");
-// let sliderBtnPrev = document.querySelector(".hero-prev");
-// let bgCount = 0;
-// let heroText = document.querySelectorAll(".hero-main");
-// let heroCaption = document.querySelectorAll(".hero-caption");
-// let heroTitle = document.querySelectorAll(".hero-title");
-// let heroDesc = document.querySelectorAll(".hero-desc");
-// let heroBtn = document.querySelectorAll(".hero-btn");
-// let heroBool = true;
-// let setHeroBg = (value) => {
-//   heroBg.style.backgroundImage = `url(./assets/img/hero/hero-${value + 1}.jpg)`;
-// };
-// let activeHero = () => {
-//   if (heroBool) {
-//     heroText[0].classList.remove("active");
-//     heroCaption[0].classList.remove("active");
-//     heroTitle[0].classList.remove("active");
-//     heroDesc[0].classList.remove("active");
-//     heroBtn[0].classList.remove("active");
-//     heroText[1].classList.add("active");
-//     heroCaption[1].classList.add("active");
-//     heroTitle[1].classList.add("active");
-//     heroDesc[1].classList.add("active");
-//     heroBtn[1].classList.add("active");
-//     heroBool = false;
-//   } else {
-//     heroText[1].classList.remove("active");
-//     heroCaption[1].classList.remove("active");
-//     heroTitle[1].classList.remove("active");
-//     heroDesc[1].classList.remove("active");
-//     heroBtn[1].classList.remove("active");
-//     heroText[0].classList.add("active");
-//     heroCaption[0].classList.add("active");
-//     heroTitle[0].classList.add("active");
-//     heroDesc[0].classList.add("active");
-//     heroBtn[0].classList.add("active");
-//     heroBool = true;
-//   }
-// };
-// setHeroBg(0);
-// sliderBtnNext.onclick = () => {
-//   activeHero();
-//   bgCount++;
-//   if (bgCount < 2) {
-//     setHeroBg(bgCount);
-//   } else {
-//     bgCount = 0;
-//     setHeroBg(bgCount);
-//   }
-// };
-// sliderBtnPrev.onclick = () => {
-//   activeHero();
-//   bgCount--;
-//   if (bgCount >= 0) {
-//     setHeroBg(bgCount);
-//   } else {
-//     bgCount = 1;
-//     setHeroBg(bgCount);
-//   }
-// };
-
-// window.onload = () => {
-//   heroText[0].classList.add("active");
-//   heroCaption[0].classList.add("active");
-//   heroTitle[0].classList.add("active");
-//   heroDesc[0].classList.add("active");
-//   heroBtn[0].classList.add("active");
-// };
-
-// // =============== Product color select ================
-
-// let colorPicker = document.querySelectorAll(".color-picker");
-// let productImg = document.querySelector(".product-item-img");
-// let colorLabel = document.querySelectorAll(".color-picker-label");
-// console.log(colorPicker);
-// colorPicker.forEach((e, index) => {
-//   colorPicker[index].addEventListener("change", () => {
-//     colorLabel[index].classList.add("active");
-//     productImg.src = `./assets/img/product/product-${index + 1}.jpg`;
-//     [...colorLabel]
-//       .filter((e, i) => i != index)
-//       .forEach((e) => {
-//         e.classList.remove("active");
-//       });
-//   });
-// });
-
-// =============== Admin ===============
-
-let tabLinks = document.getElementsByClassName("admin-nav-btn");
-let tabContent = document.getElementsByClassName("nothing");
-for(let i=0;i<tabLinks.length;i++){
-  tabLinks[i].onclick = () =>{
-    for(let j=0;j<tabContent.length;j++){
-      tabContent[j].style.display = "none";
-    }
-    for(let j=0;j<tabLinks.length;j++){
-      tabLinks[j].classList.remove("active");
-    }
-    tabLinks[i].classList.add("active");
-    tabContent[i].style.display = "block";
+// Slider
+let heroBg = document.querySelector(".hero-bg");
+let sliderBtnNext = document.querySelector(".hero-next");
+let sliderBtnPrev = document.querySelector(".hero-prev");
+let bgCount = 0;
+let heroText = document.querySelectorAll(".hero-main");
+let heroCaption = document.querySelectorAll(".hero-caption");
+let heroTitle = document.querySelectorAll(".hero-title");
+let heroDesc = document.querySelectorAll(".hero-desc");
+let heroBtn = document.querySelectorAll(".hero-btn");
+let heroBool = true;
+let setHeroBg = (value) => {
+  heroBg.style.backgroundImage = `url(./assets/img/hero/hero-${value + 1}.jpg)`;
+};
+let activeHero = () => {
+  if (heroBool) {
+    heroText[0].classList.remove("active");
+    heroCaption[0].classList.remove("active");
+    heroTitle[0].classList.remove("active");
+    heroDesc[0].classList.remove("active");
+    heroBtn[0].classList.remove("active");
+    heroText[1].classList.add("active");
+    heroCaption[1].classList.add("active");
+    heroTitle[1].classList.add("active");
+    heroDesc[1].classList.add("active");
+    heroBtn[1].classList.add("active");
+    heroBool = false;
+  } else {
+    heroText[1].classList.remove("active");
+    heroCaption[1].classList.remove("active");
+    heroTitle[1].classList.remove("active");
+    heroDesc[1].classList.remove("active");
+    heroBtn[1].classList.remove("active");
+    heroText[0].classList.add("active");
+    heroCaption[0].classList.add("active");
+    heroTitle[0].classList.add("active");
+    heroDesc[0].classList.add("active");
+    heroBtn[0].classList.add("active");
+    heroBool = true;
   }
-}
-tabLinks[0].click();
+};
+setHeroBg(0);
+sliderBtnNext.onclick = () => {
+  activeHero();
+  bgCount++;
+  if (bgCount < 2) {
+    setHeroBg(bgCount);
+  } else {
+    bgCount = 0;
+    setHeroBg(bgCount);
+  }
+};
+sliderBtnPrev.onclick = () => {
+  activeHero();
+  bgCount--;
+  if (bgCount >= 0) {
+    setHeroBg(bgCount);
+  } else {
+    bgCount = 1;
+    setHeroBg(bgCount);
+  }
+};
+
+window.onload = () => {
+  heroText[0].classList.add("active");
+  heroCaption[0].classList.add("active");
+  heroTitle[0].classList.add("active");
+  heroDesc[0].classList.add("active");
+  heroBtn[0].classList.add("active");
+};
+
+// =============== Product color select ================
+
+let colorPicker = document.querySelectorAll(".color-picker");
+let productImg = document.querySelector(".product-item-img");
+let colorLabel = document.querySelectorAll(".color-picker-label");
+console.log(colorPicker);
+colorPicker.forEach((e, index) => {
+  colorPicker[index].addEventListener("change", () => {
+    colorLabel[index].classList.add("active");
+    productImg.src = `./assets/img/product/product-${index + 1}.jpg`;
+    [...colorLabel]
+      .filter((e, i) => i != index)
+      .forEach((e) => {
+        e.classList.remove("active");
+      });
+  });
+});
